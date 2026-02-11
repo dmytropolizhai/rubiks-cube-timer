a435e077b137d2d528d63e8b3aed2057c49d7cde
Dmytro <dmytropolizhai@gmail.com>
2026-02-11 10:49:58 +0200

    fix(scramble-generator): Repeatative moves (example: R and R'  sequentially)

--- src/app/scramble/scramble-generator.ts
+++ src/app/scramble/scramble-generator.ts
@@ -4,15 +4,48 @@ import { Injectable } from "@angular/core";
     providedIn: 'root'
 })
 export class ScrambleGenerator {
-    private readonly moves = ['R', 'L', 'U', 'D', 'F', 'B'];
-    private readonly modifiers = ['', "'", '2'];
+    private readonly _moves = ['R', 'L', 'U', 'D', 'F', 'B'];
+    private readonly _modifiers = ['', "'", '2'];
+
+    private _generatedMoves: string[] = [];
+    /**
+     * Generates a random move.
+     * @returns A random move.
+     */
+    private generateMove(): string {
+        return this._moves[Math.floor(Math.random() * this._moves.length)];
+    }
+
+    /**
+     * Generates a random modifier for a move.
+     * @returns A random modifier for a move.
+     */
+    private generateModifier(): string {
+        return this._modifiers[Math.floor(Math.random() * this._modifiers.length)];
+    }
 
     generateScramble(length: number): string {
         let scramble = '';
         for (let i = 0; i < length; i++) {
-            const move = this.moves[Math.floor(Math.random() * this.moves.length)];
-            const modifier = this.modifiers[Math.floor(Math.random() * this.modifiers.length)];
-            scramble += move + modifier + ' ';
+            let move = this.generateMove();
+            let modifier = this.generateModifier();
+            let fullMove = move + modifier;
+
+            if (this._generatedMoves.length === 0) {
+                this._generatedMoves.push(fullMove);
+                scramble += fullMove + ' ';
+                continue;
+            }
+            const previousFullMove = this._generatedMoves[this._generatedMoves.length - 1];
+
+            while (previousFullMove === fullMove || previousFullMove.slice(0, 1) === move) {
+                move = this.generateMove();
+                modifier = this.generateModifier();
+                fullMove = move + modifier;
+            }
+
+            this._generatedMoves.push(fullMove);
+            scramble += fullMove + ' ';
         }
         return scramble.trim();
     }
