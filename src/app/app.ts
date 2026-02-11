6ce771b9acdfa1fcc9e5a7fa8417e2f5a3c5995d
Dmytro <dmytropolizhai@gmail.com>
2026-02-11 17:51:12 +0200

    feat: Add SolvingList to app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Stopwatch } from './stopwatch/stopwatch';
 import { Scramble } from "./scramble/scramble";
+import { SolvingList } from "./solving/solving-list/solving-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Stopwatch, Scramble],
+  imports: [RouterOutlet, Stopwatch, Scramble, SolvingList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
