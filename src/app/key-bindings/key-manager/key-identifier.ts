167cd2981bf3998a245813b036ee4561cc7e2463
Dmytro <dmytropolizhai@gmail.com>
2026-02-14 13:18:38 +0200

    refactor(key-identifier): Create KeyIdentifier

---
+++ src/app/key-bindings/key-manager/key-identifier.ts
@@ -0,0 +1,33 @@
+import { Key, Modifier } from "./types";
+
+/**
+ * Identifies keys pressed in a KeyboardEvent.
+ */
+export class KeyIdentifier {
+    private _keys: Key[] = [];
+
+    private static readonly MODIFIERS = new Map<Modifier, keyof KeyboardEvent>([
+        ["ctrl", "ctrlKey"],
+        ["shift", "shiftKey"],
+        ["alt", "altKey"],
+        ["meta", "metaKey"],
+    ]);
+
+    constructor(event: KeyboardEvent) {
+        KeyIdentifier.MODIFIERS.forEach((prop, name) => {
+            if (event[prop]) {
+                this._keys.push(name);
+            }
+        });
+
+        const mainKey = event.key.toLowerCase().replace(" ", "space");
+
+        if (![...KeyIdentifier.MODIFIERS.keys()].includes(mainKey)) {
+            this._keys.push(mainKey);
+        }
+    }
+
+    toString(): string {
+        return this._keys.join("+");
+    }
+}
--- src/app/key-bindings/key-manager/key-manager.service.ts
+++ src/app/key-bindings/key-manager/key-manager.service.ts
@@ -4,6 +4,7 @@ import { fromEvent } from "rxjs";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { CommandStore } from "./command/command.store";
 import { CommandMap } from "./types";
+import { KeyIdentifier } from "./key-identifier";
 
 
 @Injectable({
@@ -14,14 +15,15 @@ export class KeyManager {
     private readonly _destroyRef = inject(DestroyRef);
     private readonly _document = inject(DOCUMENT);
 
-
     constructor() {
         fromEvent<KeyboardEvent>(this._document, "keydown")
             .pipe(takeUntilDestroyed(this._destroyRef))
             .subscribe(event => {
                 if (event.repeat) return;
 
-                const key = this.getKeyIdentifier(event);
+                const keyIdentifier = new KeyIdentifier(event);
+                
+                const key = keyIdentifier.toString();
                 const command = this._commandStore.commands[key];
 
                 if (!command) return;
@@ -29,7 +31,6 @@ export class KeyManager {
                 if (command.preventDefault) {
                     event.preventDefault();
                 }
-
                 command.action(event);
             });
     }
@@ -47,16 +48,4 @@ export class KeyManager {
         };
     }
 
-    private getKeyIdentifier(event: KeyboardEvent): string {
-        const keys = [];
-
-        if (event.ctrlKey) keys.push("ctrl");
-        if (event.shiftKey) keys.push("shift");
-        if (event.altKey) keys.push("alt");
-        if (event.metaKey) keys.push("meta");
-
-        keys.push(event.key.toLowerCase());
-
-        return keys.join("+");
-    }
 }
