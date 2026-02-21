4c0afd1f85dcce535cf75fdf7d820bc151cd416b
Dmytro <dmytropolizhai@gmail.com>
2026-02-21 10:37:20 +0200

    feat(app): Add scramble and solve-history

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Stopwatch } from './stopwatch/stopwatch';
-import { Scramble } from "./scramble/scramble";
-import { SolvingList } from "./solving/solving-list/solving-list";
+import { StopwatchComponent } from './stopwatch';
+import { SolveHistoryComponent } from './solve/history';
+import { ScrambleComponent } from "./scramble";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Stopwatch, Scramble, SolvingList],
+  imports: [RouterOutlet, StopwatchComponent, SolveHistoryComponent, ScrambleComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('rubics-cube-timer');
-
-  startTimer() {
-    console.log('start');
-  }
 }
