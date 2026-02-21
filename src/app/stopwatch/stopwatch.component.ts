afb4d852f3fccbe4686b8313b1a1f5b0db1c93d3
Dmytro <dmytropolizhai@gmail.com>
2026-02-21 10:42:41 +0200

    refactor(stopwatch): Implement new key bindings system and StopwatchService

---
+++ src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,56 @@
+import { Component, inject } from "@angular/core";
+
+import { provideKeyBindings } from "../key-bindings";
+
+import { StopwatchHintsComponent } from "./hints";
+import { StopwatchTimeComponent } from "./time";
+
+import { StopwatchService } from "./stopwatch.service";
+
+@Component({
+    selector: "app-stopwatch",
+    template: `
+        <section class="stopwatch-container">
+            <stopwatch-time />
+            <stopwatch-hints />
+        </section>
+    `,
+    styleUrl: "./stopwatch.css",
+    imports: [StopwatchHintsComponent, StopwatchTimeComponent]
+})
+export class StopwatchComponent {
+    private readonly _stopwatch = inject(StopwatchService);
+
+    constructor() {
+        provideKeyBindings({
+            "space": {
+                description: "Start or stop the stopwatch",
+                action: {
+                    duration: 500,
+                    immediate: () => this._stopwatch.prepare(),
+                    start: () => this._stopwatch.ready(),
+                    stop: () => this.handleRelease(),
+                    cancel: () => this.handleRelease()
+                },
+                preventDefault: true
+            },
+            "control+r": {
+                action: () => this._stopwatch.reset(),
+                description: "Reset the stopwatch",
+                preventDefault: true
+            }
+        })
+    }
+
+    handleRelease() {
+        if (this._stopwatch.isReady()) {
+            this._stopwatch.start();
+        } else if (this._stopwatch.isPreparing()) {
+            this._stopwatch.reset();
+        } else if (this._stopwatch.isRunning()) {
+            this._stopwatch.stop();
+        } else if (this._stopwatch.isFinished()) {
+            this._stopwatch.reset();
+        }
+    }
+}
