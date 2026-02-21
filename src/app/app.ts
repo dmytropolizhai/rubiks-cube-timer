41bcfa403c521bdde28960a2fb4ce9241cb00299
Dmytro <dmytropolizhai@gmail.com>
2026-02-21 11:46:48 +0200

    feat(app): Implement logic to add solve

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,41 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StopwatchComponent } from './stopwatch';
-import { SolveHistoryComponent } from './solve/history';
-import { ScrambleComponent } from "./scramble";
+import { SolveHistoryComponent, SolveHistoryService } from './solve/history';
+import { ScrambleComponent, ScrambleStore } from "./scramble";
+import { StopwatchService, StopwatchComponent } from './stopwatch';
+import { SolveStatisticsComponent } from "./solve/statistics";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, StopwatchComponent, SolveHistoryComponent, ScrambleComponent],
+  imports: [RouterOutlet, StopwatchComponent, SolveHistoryComponent, ScrambleComponent, SolveStatisticsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('rubics-cube-timer');
+  protected readonly title = signal('DNF');
+
+  private readonly _stopwatchService = inject(StopwatchService);
+  private readonly _scrambleStore = inject(ScrambleStore);
+  private readonly _solveHistoryService = inject(SolveHistoryService);
+
+  constructor() {
+    this._stopwatchService.onFinish.subscribe(() => {
+      const scramble = this._scrambleStore.currentScramble();
+      const elapsedTime = this._stopwatchService.elapsedTime();
+      const today = new Date();
+
+      console.log(`Adding to solve history: 
+        Scramble: ${scramble} 
+        Elapsed time: ${elapsedTime}
+        Date: ${today}`);
+
+      this._solveHistoryService.addSolve({
+        date: today,
+        scramble: scramble,
+        elapsedTime: elapsedTime,
+      })
+
+    });
+  }
+
 }
