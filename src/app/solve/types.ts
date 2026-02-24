37f4734fea60ccda3010017fcb768cdd0dfe65b0
Dmytro <dmytropolizhai@gmail.com>
2026-02-24 19:31:53 +0200

    feat: Add penalty and formattedTime to Solve type

--- src/app/solve/types.ts
+++ src/app/solve/types.ts
@@ -1,8 +1,12 @@
 import { Scramble } from "../scramble/types";
+import { Penalty } from "./penalty";
+
 
 export type Solve = {
     id: number;
     scramble: Scramble;
+    penalty: Penalty;
     elapsedTime: number;
+    formattedTime: string;
     date: Date;
 }
