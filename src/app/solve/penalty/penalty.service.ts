8ac6d8c0b15dd7db9b15222cd4155ad6485fe5f3
Dmytro <dmytropolizhai@gmail.com>
2026-02-28 11:32:47 +0200

    feat(penalty): Implement solve history in PenaltyService

--- src/app/solve/penalty/penalty.service.ts
+++ src/app/solve/penalty/penalty.service.ts
@@ -1,15 +1,19 @@
-import { Injectable, signal } from "@angular/core";
+import { Injectable, inject, signal } from "@angular/core";
 import { Penalty } from "./types";
+import { SolveHistoryService } from "../history/solve-history.service";
 
 @Injectable({
     providedIn: "root"
 })
 export class PenaltyService {
-    private _penalty = signal<Penalty>('OK');
+    private readonly _solveHistory = inject(SolveHistoryService);
 
+    private _penalty = signal<Penalty>('OK');
     penalty = this._penalty.asReadonly();
 
     setPenalty(penalty: Penalty) {
         this._penalty.set(penalty);
+        this._solveHistory.updateCurrentSolve({ penalty });
+        console.log('Penalty set to:', this._penalty(), 'for solve with id', this._solveHistory.currentSolve().id);
     }
 }
