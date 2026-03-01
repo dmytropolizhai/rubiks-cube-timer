66224641e013914c89fd577c668c377af6b60f87
Dmytro <dmytropolizhai@gmail.com>
2026-03-01 10:01:57 +0200

    refactor(penalty-selector): move currentPenalty to input

--- src/app/solve/penalty/penalty-selector.component.ts
+++ src/app/solve/penalty/penalty-selector.component.ts
@@ -1,11 +1,11 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, input } from "@angular/core";
 import { PenaltyService } from "./penalty.service";
 import { Penalty } from "./types";
 
 @Component({
     selector: "solve-penalty-selector",
     template: `
-        <select class="primary-container" (change)="onPenaltyChange($event)">
+        <select class="primary-container" [value]="currentPenalty()" (change)="onPenaltyChange($event)">
             <option value="OK">OK</option>
             <option value="+2">+2</option>
             <option value="DNF">DNF</option>
@@ -15,10 +15,13 @@ import { Penalty } from "./types";
 export class PenaltySelector {
     private readonly _penaltyService = inject(PenaltyService);
 
+    solveId = input.required<number>();
+    currentPenalty = input<Penalty>();
+
     onPenaltyChange(event: Event) {
         const target = event.target as HTMLSelectElement;
         const penalty = target.value as Penalty;
-
-        this._penaltyService.setPenalty(penalty);
+        this._penaltyService.setPenalty(this.solveId(), penalty);
+        console.log(this.solveId(), penalty)
     }
 }
