7b3869b5bd6c057b0a3bd8e0aa2977aeadcd7581
Dmytro <dmytropolizhai@gmail.com>
2026-03-01 09:40:29 +0200

    refactor(solve-history): Add comments to every method refactor(solve-history): updateCurrentSolve change logic to update current solve

--- src/app/solve/history/solve-history.service.ts
+++ src/app/solve/history/solve-history.service.ts
@@ -1,5 +1,6 @@
 import { computed, Injectable, signal } from "@angular/core";
 import { Solve } from "../types";
+import { Penalty } from "../penalty";
 
 @Injectable({
     providedIn: 'root'
@@ -10,6 +11,23 @@ export class SolveHistoryService {
 
     currentSolve = computed(() => this._solves.asReadonly()()[0]);
 
+    /**
+     * Formats the elapsed time in milliseconds to a string representation.
+     * @param timeMs The elapsed time in milliseconds.
+     * @param penalty The penalty to apply to the elapsed time.
+     * @returns The formatted time as a string.
+     */
+    private formatTime(timeMs: number, penalty: Penalty) {
+        if (penalty === "DNF") return "DNF";
+        const totalMs = penalty === "+2" ? timeMs + 2000 : timeMs;
+
+        return (totalMs / 1000).toFixed(2);
+    }
+
+    /**
+     * Adds a new solve to the solve history.
+     * @param solve The solve to add to the solve history.
+     */
     addSolve(solve: Omit<Solve, 'id' | 'formattedTime'>) {
         console.log(`Adding to solve history: 
             Scramble: ${solve.scramble} 
@@ -20,18 +38,31 @@ export class SolveHistoryService {
 
         const newSolve: Solve = {
             id: this._solves().length + 1,
-            formattedTime: (solve.elapsedTime / 1000).toFixed(2),
+            formattedTime: this.formatTime(solve.elapsedTime, solve.penalty),
             ...solve,
         };
 
         this._solves.update((solves) => [newSolve, ...solves]);
     }
 
-    updateCurrentSolve(solve: Partial<Solve>) {
-        this._solves.update((solves) => {
-            const lastSolve = solves[solves.length - 1];
-            const updatedSolve = { ...lastSolve, ...solve };
-            return [...solves.slice(0, -1), updatedSolve];
-        });
+    /**
+     * Updates the penalty for the current solve.
+     * @param penalty The penalty to apply to the current solve.
+     */
+    updateCurrentSolvePenalty(penalty: Penalty) {
+        this._solves.update(solves => {
+            if (solves.length === 0) return solves;
+
+            const current = solves[0]
+
+            const updatedSolve = {
+                ...current,
+                penalty,
+                formattedTime: this.formatTime(current.elapsedTime, penalty)
+            }
+
+            return [updatedSolve, ...solves.slice(1)];
+        })
     }
+
 }
