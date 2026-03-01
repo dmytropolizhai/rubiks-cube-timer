3a47ef1956e1b7b8920f5d0e9e81421f643c790c
Dmytro <dmytropolizhai@gmail.com>
2026-03-01 09:40:51 +0200

    refactor(penalty-service): Implement new updateCurrentSolvePenalty method in setPenalty

--- src/app/solve/penalty/penalty.service.ts
+++ src/app/solve/penalty/penalty.service.ts
@@ -1,19 +1,19 @@
 import { Injectable, inject, signal } from "@angular/core";
 import { Penalty } from "./types";
 import { SolveHistoryService } from "../history/solve-history.service";
+import { Solve } from "../types";
 
 @Injectable({
     providedIn: "root"
 })
 export class PenaltyService {
     private readonly _solveHistory = inject(SolveHistoryService);
-
     private _penalty = signal<Penalty>('OK');
+
     penalty = this._penalty.asReadonly();
 
     setPenalty(penalty: Penalty) {
         this._penalty.set(penalty);
-        this._solveHistory.updateCurrentSolve({ penalty });
-        console.log('Penalty set to:', this._penalty(), 'for solve with id', this._solveHistory.currentSolve().id);
+        this._solveHistory.updateCurrentSolvePenalty(penalty);
     }
 }
